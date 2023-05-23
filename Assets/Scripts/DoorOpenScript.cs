45a4385aab025815ae74e9a3083b6c813ee645f3
oTuff <oscartuff@protonmail.com>
2023-05-23 15:24:38 +0200

    edit DoorOpenScript and added opening of doors in Night Club scene

--- Assets/Scripts/DoorOpenScript.cs
+++ Assets/Scripts/DoorOpenScript.cs
@@ -8,14 +8,17 @@ public class DoorOpenScript : MonoBehaviour
     private GameObject doorRight;
     [SerializeField]
     private GameObject doorLeft;
+    [SerializeField]
+    private float doorOpenAmount = 5;
+    
     private bool isOpen;
 
     private void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.CompareTag("Player") && !isOpen)
         {
-        doorRight.transform.Translate(Vector3.right);
-        doorLeft.transform.Translate(Vector3.left);
+        doorRight.transform.Translate(Vector3.right*doorOpenAmount);
+        doorLeft.transform.Translate(Vector3.left*doorOpenAmount);
         isOpen = true;
         }
     }
