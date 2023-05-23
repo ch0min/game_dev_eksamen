0f3c31e571133565bec9971108a3fe8b6502bd0e
oTuff <oscartuff@protonmail.com>
2023-05-23 11:54:58 +0200

    added triggers to open/close doors

---
+++ Assets/Scripts/DoorCloseScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorCloseScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject doorRight;
+    [SerializeField]
+    private GameObject doorLeft;
+
+    private bool isOpen = true;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (isOpen){
+        doorRight.transform.Translate(Vector3.left);
+        doorLeft.transform.Translate(Vector3.right);
+        isOpen = false;
+        }
+    }
+}
---
+++ Assets/Scripts/DoorOpenScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject doorRight;
+    [SerializeField]
+    private GameObject doorLeft;
+
+    private bool isOpen;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (!isOpen){
+        doorRight.transform.Translate(Vector3.right);
+        doorLeft.transform.Translate(Vector3.left);
+        isOpen = true;
+        }
+    }
+}
