5e7d6c259089cc7b57b12ac16da1d2991629a9b4
Ch0min <marklundgaard7@gmail.com>
2023-05-23 15:09:16 +0200

    Character Movement and Animation

--- Assets/Scripts/BulletProjectile.cs
+++ Assets/Scripts/BulletProjectile.cs
@@ -1,25 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletProjectile : MonoBehaviour
 {
+    [SerializeField]
+    Transform cameraPosition;
+    
     public GameObject bullet;
     public float bulletForce = 2000f;
-    
+    public float fireRate = 0.5f;
+    float nextFiretime = 0f;
 
-    void Update()
-    {
-        if(Input.GetButtonDown("Fire1")) {
+
+    public GameObject muzzleFlash;
+    public Transform muzzleFlashPosition;
+
+
+    void Update() {
+        Shoot();
+
+
+        // if (Input.GetMouseButtonDown(0)) {
+        //     muzzleFlash.Play();
+        // }
+        // else {
+        //     muzzleFlash.Stop();
+        // }
+    }
+
+
+    void Shoot() {
+        
+        if (Input.GetButton("Fire1") && Time.time >= nextFiretime) {
+            nextFiretime = Time.time + fireRate;
+            GameObject Flash = Instantiate(muzzleFlash, muzzleFlashPosition);
+
+            
             GameObject bulletHolder;
             bulletHolder = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
-            bulletHolder.transform.Rotate(Vector3.left * 90);   // Sometimes needed, depends on the rotation of the Weaponholder.
+            bulletHolder.transform.Rotate(Vector3.left * 90); // Sometimes needed, depends on the rotation of the Weaponholder.
 
             Rigidbody _rigidbody;
             _rigidbody = bulletHolder.GetComponent<Rigidbody>();
             _rigidbody.AddForce(transform.forward * bulletForce);
             
+            
+
+            Destroy(Flash, 0.15f);
+
             Destroy(bulletHolder, 3.0f);
+
         }
+        
     }
+    
 }
